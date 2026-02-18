3a9c506b6498386efd62e5f375bee7f2379763cd
rid1matin <ridonematin@gmail.com>
2026-02-18 17:12:39 -0500

    feature: footer created

---
+++ src/client/src/components/Footer.jsx
@@ -0,0 +1,25 @@
+export default function Footer() {
+    return (
+        <footer>
+            <div>
+
+                <div class="footer-left">
+                    <h2 class="logo">TaskScheduler</h2>
+                    <p>Plan smarter. Achieve faster.</p>
+                </div>
+
+                <div class="footer-center">
+                    <a href="#">Home</a>
+                    <a href="#">Tasks</a>
+                    <a href="#">Calendar</a>
+                    <a href="#">About</a>
+                </div>
+
+                <div class="footer-right">
+                    <p id="copyright"></p>
+                </div>
+
+            </div>
+        </footer>
+    )
+}
