47a8c43fda48d9776992c0be8de994bfe7fc60c3
rid1matin <ridonematin@gmail.com>
2026-02-25 15:26:02 -0500

    feature: blank test page

--- src/client/src/App.jsx
+++ src/client/src/App.jsx
@@ -1,13 +1,5 @@
 import SidebarLayout from "./components/SidebarLayout";
-import "./App.css";
 
-function App() {
-  return (
-    <SidebarLayout>
-      <h1>Vite + React</h1>
-      <p>Your main page content goes here.</p>
-    </SidebarLayout>
-  );
+export default function App() {
+  return <SidebarLayout>{/* blank page for now */}</SidebarLayout>;
 }
-
-export default App;
